bd94f49a544cec4b8d364f26c724b1c5d7f86ecc
Janaina Arque <janainarque@gmail.com>
2023-05-23 21:07:57 -0300

    ultima atividade de laço

--- Ultima atividade laço/src/principal/Program.java
+++ Ultima atividade laço/src/principal/Program.java
@@ -12,23 +12,25 @@ public class Program {
         System.out.print("Digite um tipo de operação seja ela soma, divisão, multiplicação, subtração: ");
         String operacao = sc.nextLine();
         System.out.print("Digite o número: ");
-        int numero = sc.nextInt();
+        int num = sc.nextInt();
         
         for (int i = 1; i <= 10; i++) {
             if (operacao.equals("soma")) {
-                System.out.println(numero + " + " + i + " = " + (numero + i));
-            } else if (operacao.equals("divisão")) {
-                System.out.println(numero + " / " + i + " = " + ((double) numero / i));
-            } else if (operacao.equals("multiplicação")) {
-                System.out.println(numero + " * " + i + " = " + (numero * i));
+                System.out.println(num + " + " + i + " = " + (num + i));
             } else if (operacao.equals("subtração")) {
-                System.out.println(numero + " - " + i + " = " + (numero - i));
+                System.out.println((num + i) + " - " + num + " = " + i);
+            } else if (operacao.equals("multiplicação")) {
+                System.out.println(num + " * " + i + " = " + (num * i));
+            } else if (operacao.equals("divisão")) {
+                System.out.println((num * i) + " / " + num + " = " + i);
             }
         }
-        
+	
         
         sc.close();
 		
 	}
+
+	
 
 }
