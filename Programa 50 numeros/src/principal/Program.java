ede002fff7a0adf6bf3f171cc08f7451fc07d131
Janaina Arque <janainarque@gmail.com>
2023-05-25 21:51:22 -0300

    aula de array, aula de matriz e exercicios de matriz e array

--- Programa 50 numeros/src/principal/Program.java
+++ Programa 50 numeros/src/principal/Program.java
@@ -9,18 +9,16 @@ public class Program {
 		// TODO Auto-generated method stub
 		
 		Scanner sc = new Scanner(System.in);
-		Random random = new Random();
         
-        int[] numeros = new int[51];
+        int[] numeros = new int[50];
         
-        for (int i = 1; i < 51; i++) {
+        for (int i = 1; i < 50; i++) {
             System.out.print("Digite o número " + (i) + ": ");
             numeros[i] = sc.nextInt();
-        	//numeros[i] = random.nextInt(50);
         }
         
         
-        for (int i = 1; i < 51; i++) {
+        for (int i = 1; i < 50; i++) {
             if (numeros[i] % 2 == 0) {
                 System.out.println("Número par é o: " + numeros[i] + ", Posição: " + (i));
             }
