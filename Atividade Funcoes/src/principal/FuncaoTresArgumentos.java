ddca39787cb3d9a2d8a6afc24a329576e5646340
Janaina Arque <janainarque@gmail.com>
2023-05-30 20:55:38 -0300

    Atividade Funções

--- Atividade Funcoes/src/principal/FuncaoTresArgumentos.java
+++ Atividade Funcoes/src/principal/FuncaoTresArgumentos.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class FuncaoTresArgumentos {
 
-
+/* 
+ * meu codigo 
+ * 
 	public static void main(String[] args) {
 			
 		Scanner sc = new Scanner(System.in); 
@@ -21,12 +23,39 @@ public class FuncaoTresArgumentos {
 		System.out.println();
 		System.out.println(somaNumeros(numero1, numero2, numero3));
 		
+	
+		sc.close();
+		
+	}
+	
+	
+	public static String somaNumeros(double numero1, double numero2, double numero3) {
+		return "A soma dos desses três argumentos foi: " + (numero1 + numero2 + numero3); 			
+	}
+	
+*/
+	
+	
+	//Código Professor
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); 
+		
+		System.out.println("Entre com os 3 números a serem somados");
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int n3 = sc.nextInt();
+		
+		somarNumeros(n1, n2, n3);
+		
+		
 		sc.close();
 		
 	}
 
-	public static String somaNumeros(double numero1, double numero2, double numero3) {
-		return "A soma dos desses três argumentos foi: " + (numero1 + numero2 + numero3); 			
+	static void somarNumeros(int n1, int n2, int n3) {
+		System.out.println("A soma dos numeros é: ");
+		System.out.println(n1 + n2 + n3);
 	}
 		
 		
