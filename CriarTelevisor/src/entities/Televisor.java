13424373f03175f2d653540d2bef7ac410f3d61f
Janaina Arque <janainarque@gmail.com>
2023-06-01 21:48:21 -0300

    Atividade de classe e metodo 2

---
+++ CriarTelevisor/src/entities/Televisor.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Televisor {
+	
+	public double aumentarVolume;
+	public double reduzirVolume;
+	public double subirCanal;
+	public double descerCanal;
+	public double lugarTelevisor;
+	public double desligarTelevisor;
+	public double mostrarStatus;
+
+}
