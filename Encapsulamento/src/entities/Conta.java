81b7811309a75d3c13291ddb9200fcec4209b043
Janaina Arque <janainarque@gmail.com>
2023-06-05 21:25:02 -0300

    atividade reforço

---
+++ Encapsulamento/src/entities/Conta.java
@@ -0,0 +1,51 @@
+package entities;
+
+public class Conta {
+	
+	public int numeroConta;
+	public int agencia;
+	public int salario;
+	public double saldo;
+	public String titular;
+	
+	public void depositar(double valorDepositado) {
+		saldo += valorDepositado;
+	}
+
+	public void sacar(double valorSacado) {
+		saldo -= valorSacado;
+	}
+
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public int getSalario() {
+		return salario;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+
+}
---
+++ Encapsulamento/src/principal/Program.java
@@ -0,0 +1,44 @@
+package principal;
+
+import java.util.Scanner;
+
+import entities.Conta;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Conta conta = new Conta();
+		
+		System.out.println("Titular: ");
+		conta.setTitular(sc.nextLine()); 
+		
+		System.out.println("Agência: ");
+		conta.setAgencia(sc.nextInt());
+		
+		System.out.println("Número conta: ");
+		conta.setNumeroConta(sc.nextInt());
+		
+		/* System.out.println("Saldo: ");
+		conta.saldo = sc.nextDouble(); */
+		
+		System.out.println("Entre com o valor a depositar: ");
+		double deposito = sc.nextDouble();
+		
+		//conta.setSaldo(5000);
+		conta.depositar(deposito);
+		
+		System.out.println("Entre com o valor a sacar: ");
+		double sacar = sc.nextDouble();
+		
+		conta.sacar(sacar);
+		
+		System.out.println("Saldo : " + conta.getSaldo());
+		
+		
+		sc.close();
+
+	}
+
+}
