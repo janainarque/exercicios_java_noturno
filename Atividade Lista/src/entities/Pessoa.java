566bdbe10ee0ed8e6a4958daa67c1e5724a3718e
Janaina Arque <janainarque@gmail.com>
2023-06-14 21:35:20 -0300

    Atividade de Lista

---
+++ Atividade Lista/src/entities/Pessoa.java
@@ -0,0 +1,44 @@
+package entities;
+
+public class Pessoa {
+	
+	private String nome;
+	private int idade;
+	private String telefone;
+	
+	
+	public Pessoa(String nome, int idade, String telefone) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.telefone = telefone;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	} 
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "nome: " + nome +
+				"\nTelefone: " + telefone +
+				"\nIdade: " + idade;
+	}
+
+}
---
+++ Atividade Lista/src/principal/Program.java
@@ -0,0 +1,52 @@
+package principal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.Pessoa;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Pessoa pessoa; //chama a variavel pessoa da classe Pessoa
+		List<Pessoa> pessoas = new ArrayList<>();
+		char opcao;
+		
+		do {
+			
+			System.out.print("Entre com o nome: ");
+			//pessoa.setNome(sc.nextLine());
+			String nome = sc.nextLine();
+			
+			System.out.print("Entre com o telefone: ");
+			//pessoa.setTelefone(sc.nextLine());
+			String telefone = sc.nextLine();
+			
+			System.out.print("Entre com a idade: ");
+			//pessoa.setIdade(sc.nextInt());
+			int idade = sc.nextInt();
+			
+			
+			
+			pessoa = new Pessoa(nome, idade, telefone);
+			
+			pessoas.add(pessoa);
+			System.out.print("Deseja cadastrar mais uma pessoa? ");
+			System.out.println("s/n");
+			opcao = sc.next().charAt(0);
+			sc.nextLine();
+		}while(Character.toLowerCase(opcao) == 's');
+		
+		System.out.println("Pessoas cadastradas: ");
+		for(Pessoa p : pessoas) {
+			System.out.println(p + "\n");
+		}
+		
+		sc.close();
+
+	}
+
+}
